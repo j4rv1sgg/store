3f1c45a925ed551f85abaf794ee7eafd9d8390a3
j4rv1sgg <jrvs.inc@gmail.com>
2024-11-20 12:42:36 +0100

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start"></div>
+      <div className="navbar-center">
+        <a className=" text-xl">FakeStore</a>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+}
